3dd5861aaa31164110e744f46a99a174e6ef2766
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 15:18:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|food| cheese_types.includ?(food)} 
+  array.find {|food| cheese_types.include?(food)} 
 end
 
 

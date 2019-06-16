df635e5efd96a750386493502185d2da90bec077
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 14:54:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ array = ["wooooooooo hooooo", "Hallelujah"]
 def long_planeteer_calls(array)
   new_array = array.map {|name| name.length} 
   puts new_array
-  array.include?(3..1000) ? true : false
+  new_array.include?(3..1000) ? true : false
 end
 
 def find_the_cheese# code an argument here

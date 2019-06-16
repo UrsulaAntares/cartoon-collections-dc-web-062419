73fce7a963f00db9ccfc5b32eb082a1a59e13a59
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 14:34:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map {|name| name.capitalize << "!"}
 end
 
-def long_planeteer_calls(calls_long)
-  calls_long.include? { |word| word.legnth >4}
+def long_planeteer_calls(array)
+  array.include? { |word| word.legnth >4}
 end
 
 def find_the_cheese# code an argument here
